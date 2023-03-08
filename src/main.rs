85bef84f6fbb7a03787c6011e95058ea99249ff6
Maciej Urban <maciej-urban@outlook.com>
2023-03-08 16:30:32 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+use color_eyre::Report;
+use template_rs::setup::setup;
+use tracing::info;
+
+#[derive(Parser, Debug)]
+#[command(name = "synker", version = "0.1.0", author = "Maciej Urban")]
+pub struct Args {
+    #[arg(short, long, default_value = "config.toml")]
+    config: String,
+}
+
+fn main() -> Result<(), Report> {
+    setup()?;
+    let args = Args::parse();
+    info!(args.config);
+
+    Ok(())
+}
