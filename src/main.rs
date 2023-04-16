fc39e7ea2e8bc706601fd7f03704c64e04cc9fe9
Maciej Urban <maciej-urban@outlook.com>
2023-04-16 10:14:43 +0200

    added shorthand versions

--- src/main.rs
+++ src/main.rs
@@ -10,43 +10,42 @@ fn main() -> Result<(), Report> {
     let matches = cli().get_matches();
     let mut base_command = process::Command::new("git");
 
-    if let Some(("go-back", sub_matches)) = matches.subcommand() {
-        let num_of_commits = sub_matches.get_one::<String>("n").expect("required");
-        info!("Going back {num_of_commits} commits");
-        base_command
-            .arg("reset")
-            .arg("HEAD~".to_owned() + num_of_commits);
-    }
-
-    if let Some(("clean", _)) = matches.subcommand() {
-        info!("Running git clean -fdx");
-        base_command.arg("clean").arg("-fdx");
-    }
-
-    if let Some(("reset", _)) = matches.subcommand() {
-        info!("Running git reset --hard");
-        base_command.arg("reset").arg("--hard");
-    }
-
-    if let Some(("ff", _)) = matches.subcommand() {
-        info!("Running git pull");
-        base_command.arg("pull");
-    }
-
-    if let Some(("over", sub_matches)) = matches.subcommand() {
-        let change_msg = sub_matches
-            .get_one::<String>("change_msg")
-            .expect("required");
-
-        let should_change = matches!(change_msg.as_str(), "yes");
-        info!(
-            "Overwriting previous commit with current changes, new commit message: {should_change}"
-        );
-        base_command.arg("commit").arg("-a").arg("--amend");
-
-        if !should_change {
-            base_command.arg("--no-edit");
+    match matches.subcommand() {
+        Some(("go-back", sub_matches)) | Some(("-g", sub_matches)) => {
+            let num_of_commits = sub_matches.get_one::<String>("n").expect("required");
+            info!("Going back {num_of_commits} commits");
+            base_command
+                .arg("reset")
+                .arg("HEAD~".to_owned() + num_of_commits);
         }
+        Some(("clean", _)) | Some(("-c", _)) => {
+            base_command.arg("clean").arg("-fdx");
+            info!("Running git clean -fdx");
+        }
+        Some(("reset", _)) | Some(("-r", _)) => {
+            info!("Running git reset --hard");
+            base_command.arg("reset").arg("--hard");
+        }
+        Some(("ff", _)) | Some(("-f", _)) => {
+            info!("Running git pull");
+            base_command.arg("pull");
+        }
+        Some(("over", sub_matches)) | Some(("-o", sub_matches)) => {
+            let change_msg = sub_matches
+                .get_one::<String>("change_msg")
+                .expect("required");
+
+            let should_change = matches!(change_msg.as_str(), "yes");
+            info!(
+                "Overwriting previous commit with current changes, new commit message: {should_change}"
+            );
+            base_command.arg("commit").arg("-a").arg("--amend");
+
+            if !should_change {
+                base_command.arg("--no-edit");
+            }
+        }
+        _ => {}
     }
 
     base_command.spawn()?.wait()?;
@@ -61,21 +60,26 @@ fn cli() -> Command {
         .allow_external_subcommands(true)
         .subcommand(
             Command::new("go-back")
+                .short_flag('g')
                 .about("Go back n number of commits without removing files created in commits")
                 .arg(arg!(<n> "Number of commits")),
         )
-        .subcommand(Command::new("clean").about(
+        .subcommand(Command::new("clean").short_flag('c').about(
             "Clean current directory out of all files that are not tracked and that are ignored",
         ))
         .subcommand(
-            Command::new("reset").about("Reset current directory to the state of the last commit"),
+            Command::new("reset")
+                .short_flag('r')
+                .about("Reset current directory to the state of the last commit"),
         )
         .subcommand(
             Command::new("ff")
+                .short_flag('f')
                 .about("Fast forward current branch to the latest commit on that branch"),
         )
         .subcommand(
             Command::new("over")
+                .short_flag('o')
                 .about("Overwrite last commit with current changes")
                 .arg(
                     arg!(<change_msg> "Should commit message be changed")
